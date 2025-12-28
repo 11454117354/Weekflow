5bfec53b33c3b5308aceeea81c44c2ff5f9a797d
陈 . <chen.@chendebijibendiannao.local>
2025-12-28 14:27:06 +0800

    Added a A button for archive

--- static/index.js
+++ static/index.js
@@ -10,7 +10,8 @@
         const weeklist = document.getElementById("week-list");
 
         weeklist.innerHTML = "";
-
+        
+        // week<li>
         weeks.forEach(week => {
             const li = document.createElement("li");
             li.classList.add("week-item");
@@ -21,12 +22,17 @@
 
             const renameBtn = document.createElement("button");
             renameBtn.innerText = "R";
-            renameBtn.classList.add("rename-btn", "button-group");
+            renameBtn.classList.add("rename-btn");
+
+            const archiveBtn = document.createElement("button");
+            archiveBtn.innerText = "A";
+            archiveBtn.classList.add("archive-btn");
 
             const deleteBtn = document.createElement("button");
             deleteBtn.innerText = "D";
-            deleteBtn.classList.add("delete-btn", "button-group");
+            deleteBtn.classList.add("delete-btn");
 
+            // rename button popup page
             renameBtn.addEventListener("click", e => {
                 e.stopPropagation();
                 const template = document.getElementById("week-popup-template");
@@ -80,6 +86,44 @@
                 console.log("rename week", week.id);
             })
 
+            // archive button settings
+            archiveBtn.addEventListener("click", async e => {
+                e.stopPropagation();
+
+                try {
+                    if (week.archived == false){
+                        const response = await fetch(`/api/weeks/${week.id}/archived`, {
+                            method: "PATCH",
+                            credentials: "same-origin",
+                            headers: {"Content-Type": "application/json"},
+                            body: JSON.stringify({ archived: true })
+                        });
+
+                        if (!response.ok) {
+                            throw new Error("Failed to archive week")
+                        }
+                        location.reload();
+                    }else if(week.archived == true){
+                        const response = await fetch(`/api/weeks/${week.id}/archived`, {
+                            method: "PATCH",
+                            credentials: "same-origin",
+                            headers: {"Content-Type": "application/json"},
+                            body: JSON.stringify({ archived: false })
+                        });
+
+                        if (!response.ok) {
+                            throw new Error("Failed to archive week")
+                        }
+                        location.reload();
+                    }
+                } catch (error) {
+                        console.error(error);
+                        alert("Error renaming week");
+                }
+                console.log("archived/unarchived a week", week.id);
+            })
+
+            // delete button settings
             deleteBtn.addEventListener("click", async e => {
                 e.stopPropagation();
 
@@ -104,6 +148,7 @@
 
             li.appendChild(nameSpan);
             li.appendChild(renameBtn);
+            li.appendChild(archiveBtn);
             li.appendChild(deleteBtn);
 
             weeklist.appendChild(li);
@@ -163,8 +208,8 @@ document.getElementById("confirm").onclick = async () => {
 
 
 
-// 1.在weeks的下面 新建week按钮
-// 2.删去navbar中的week
+// 1.在weeks的下面 新建week按钮 ✅
+// 2.删去navbar中的week ✅
 // 3.category一个dropdown menu，底下常驻一个新建
 // 4.每个week加一个归档、一个删除按钮、一个重命名，归档后没清空的自动归到下一周（新api），已归档的放入下面折叠起来
 // 5.tasks
